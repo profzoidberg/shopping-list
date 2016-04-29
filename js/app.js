45c2924e9fbdef2b06260c0faf630b9a727af152
profzoidberg <ryanlynn523@gmail.com>
2016-04-29 16:30:25 -0400

    more edits

--- js/app.js
+++ js/app.js
@@ -10,19 +10,22 @@ $(document).ready(function(){
 	function addItemToList(){
 		var input=$('#text').val();
 		if (input !== "") { 
-			$('.list').append('<li><input type="checkbox"/> '+input+' </li>');
+			$('.list').append('<li><input type="checkbox"/> '+input+' <button class="button">Done</button> </li>');
 			$('#text').val("");
 		}
 	}
 	$('.list').on('click','li', function(){ 
-		console.log( $(':checkbox').is(':checked') )
        if ($(':checkbox').is(':checked')) {
-       		console.log('checked')
        		$(this).css({'color':'gray', 'font-style': 'italic'});
        	} else { 
        		$(this).css({'color': 'black', 'font-style': 'normal'});
        	}
     })
+    $('.list').on('click','li', function(){
+    	if ($('.button').is('clicked')); {
+    		$(this).empty();
+    	}
+    });
   	$('.list-complete').click(function(){
   		$('.list li').empty();
   	});
